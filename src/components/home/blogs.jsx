ff39b2e835d3bfe9d9c27cbb880a375d0f5d9879
Silu2003 <silu.das@credennz.com>
2024-12-03 21:56:04 +0530

    first commit

---
+++ src/components/home/blogs.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+export default function blogs() {
+  return (
+    <div className="scroller-blog">
+      <div class="scroll-container">
+        <h6 class="subtitle">Industry Insights</h6>
+        <h1 id="sub-text">Our Blogs and Articles</h1>
+        <div class="blog">
+          <div class="blog-card">
+            <div class="blog-image"></div>
+            <div class="blog-content">   
+              <h2 class="blog-title">Lorem Ipsum</h2>
+              <p class="blog-description">
+                Witness the impact of our SEO strategies as we unveil the rise
+                in organic traffic...
+              </p>
+              <a href="#" class="read-more">
+                Read more <i class="fa-solid fa-arrow-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="blog-card">
+            <div class="blog-image"></div>
+            <div class="blog-content">
+              <h2 class="blog-title">Lorem Ipsum</h2>
+              <p class="blog-description">
+                Witness the impact of our SEO strategies as we unveil the rise
+                in organic traffic...
+              </p>
+              <a href="#" class="read-more">
+                Read more →
+              </a>
+            </div>
+          </div>
+          <div class="blog-card">
+            <div class="blog-image"></div>
+            <div class="blog-content">
+              <h2 class="blog-title">Lorem Ipsum</h2>
+              <p class="blog-description">
+                Witness the impact of our SEO strategies as we unveil the rise
+                in organic traffic...
+              </p>
+              <a href="#" class="read-more">
+                Read more →
+              </a>
+            </div>
+          </div>
+          <div class="blog-card">
+            <div class="blog-image"></div>
+            <div class="blog-content">
+              <h2 class="blog-title">Lorem Ipsum</h2>
+              <p class="blog-description">
+                Witness the impact of our SEO strategies as we unveil the rise
+                in organic traffic...
+              </p>
+              <a href="#" class="read-more">
+                Read more →
+              </a>
+            </div>
+          </div>
+          <div class="blog-card">
+            <div class="blog-image"></div>
+            <div class="blog-content">
+              <h2 class="blog-title">Lorem Ipsum</h2>
+              <p class="blog-description">
+                Witness the impact of our SEO strategies as we unveil the rise
+                in organic traffic...
+              </p>
+              <a href="#" class="read-more">
+                Read more →
+              </a>
+            </div>
+          </div>
+          <div class="blog-card">
+            <div class="blog-image"></div>
+            <div class="blog-content">
+              <h2 class="blog-title">Lorem Ipsum</h2>
+              <p class="blog-description">
+                Witness the impact of our SEO strategies as we unveil the rise
+                in organic traffic...
+              </p>
+              <a href="#" class="read-more">
+                Read more →
+              </a>
+            </div>
+          </div>
+          <div class="blog-card">
+            <div class="blog-image"></div>
+            <div class="blog-content">
+              <h2 class="blog-title">Lorem Ipsum</h2>
+              <p class="blog-description">
+                Witness the impact of our SEO strategies as we unveil the rise
+                in organic traffic...
+              </p>
+              <a href="#" class="read-more">
+                Read more →
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
